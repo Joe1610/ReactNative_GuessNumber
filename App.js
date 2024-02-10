491a00d96e6aa6086ac2fb0acd4eb09aa0f52508
Youssef <joemohammed157@gmail.com>
2024-02-10 09:29:44 +0200

    add "start game" and "game" screens

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import {View, Text} from 'react-native';
+import {View, ImageBackground, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import Colors from './constants/Colors';
 
 function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+    setGameIsOver(false);
+  };
+
+  const gameOverHandler = () => {
+    setGameIsOver(true);
+  };
+
+  let content = <StartGameScreen confirmNumber={startGameHandler} />;
+  if (userNumber && !gameIsOver) {
+    content = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+  // else if (gameIsOver && userNumber) {
+  //   content = <GameOverScreen />;
+  // }
+
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!!</Text>
+    <View
+      style={styles.rootScreen}
+      colors={[Colors.primary700, Colors.accent500]}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        style={styles.rootScreen}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}>
+        <SafeAreaView style={styles.rootScreen}>{content}</SafeAreaView>
+      </ImageBackground>
     </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
---
+++ screens/GameScreen.js
@@ -0,0 +1,91 @@
+import {View, StyleSheet, Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Title from '../components/ui/Title';
+import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
+import Card from '../components/ui/Card';
+import InstructionText from '../components/ui/InstructionText';
+
+const generateRandomNumber = (min, max, exclude) => {
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  if (randomNumber === exclude) {
+    return generateRandomNumber(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+};
+
+let minBound = 1;
+let maxBound = 100;
+
+const GameScreen = ({userNumber, onGameOver}) => {
+  const initialGuess = generateRandomNumber(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver]);
+
+  const nextGuessHandler = direction => {
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", 'You know that this is wrong...', [
+        {text: 'Sorry!', style: 'cancel'},
+      ]);
+      return;
+    }
+
+    if (direction === 'lower') {
+      maxBound = currentGuess;
+    } else {
+      minBound = currentGuess + 1;
+    }
+    const nextGuess = generateRandomNumber(minBound, maxBound, currentGuess);
+    setCurrentGuess(nextGuess);
+  };
+
+  return (
+    <View style={styles.screenContainer}>
+      <Title>AI Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card>
+        <InstructionText style={styles.instructionText}>
+          Higher or Lower?
+        </InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton
+              onPress={nextGuessHandler.bind(this, 'lower')}></PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+              +
+            </PrimaryButton>
+          </View>
+        </View>
+      </Card>
+    </View>
+  );
+};
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 24,
+  },
+  instructionText: {
+    marginBottom: 12,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
